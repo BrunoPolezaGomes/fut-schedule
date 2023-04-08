79ec2030be5e84c6936442f3e46a55cee5e83112
Bruno Gomes <brunoshy@gmail.com>
2023-04-08 15:24:33 -0300

    main: init new linux env

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
+    pass
 
-
-SECRET_KEY = 'sua_chave_secreta_aqui'
+SECRET_KEY = "QetsdFgHjKlMnOpQrStUvWxYz"
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'garra'
 MYSQL_PASSWORD = 'garra'
@@ -9,7 +9,7 @@ MYSQL_DB = 'garra'
 
 
 class DevelopmentConfig(Config):
-
+    pass
 
 DEBUG = True
 OPENAI_KEY = 'sua_chave_api_openai_aqui'
